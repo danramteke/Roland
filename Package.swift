0e4da64d6145e016e8ee31aac629ce97fbe3d021
Daniel <daniel@danramteke.com>
2020-11-28 18:05:54 -0500

    leaf kit dependency

--- Package.swift
+++ Package.swift
@@ -13,9 +13,14 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/iwasrobbed/Down.git",  from: "0.9.0"),
     .package(url: "https://github.com/apple/swift-argument-parser.git",  from: "0.3.0"),
+    .package(url: "https://github.com/vapor/leaf-kit.git",  from: "1.0.0"),
   ],
   targets: [      
     .target(name: "Roland", 
-            dependencies: ["Down", .product(name: "ArgumentParser", package: "swift-argument-parser")],
+            dependencies: [
+              "Down", 
+              .product(name: "ArgumentParser", package: "swift-argument-parser"),
+              .product(name: "LeafKit", package: "leaf-kit"),
+            ],
             path: "roland"),
   ])
